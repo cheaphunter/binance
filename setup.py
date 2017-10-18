f7838cd7db9b8b4cabe4f043153b12def2ee7f2e
Takaki Oshima <takakioshima@gmail.com>
2017-10-18 15:35:48 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='binance',
+      version='0.1',
+      py_modules=['binance'],
+      description='Binance API wrapper',
+      url='https://github.com/toshima/binance',
+      author='Takaki Oshima')
