50398c5ca003880cad245a14b3dadffad2e4194b
Takaki Oshima <takakioshima@gmail.com>
2017-10-19 12:11:20 +0100

    Increase version number to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='binance',
-      version='0.1',
+      version='0.2',
       py_modules=['binance'],
       description='Binance API wrapper',
       url='https://github.com/toshima/binance',
